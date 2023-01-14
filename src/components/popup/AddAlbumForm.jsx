97a61a86fba674b94b9f2a0777885bcda70bc87f
dimalka-heshan <dimalkarathnayake10@gmail.com>
2023-01-14 06:05:52 +0530

    Add,edit album changes

--- src/components/popup/AddAlbumForm.jsx
+++ src/components/popup/AddAlbumForm.jsx
@@ -7,10 +7,6 @@ const AddAlbumForm = ({ onHide }) => {
   const [genre, setGenre] = useState("");
   const [releaseDate, setReleaseDate] = useState("");
   const [genres, setGenres] = useState([]);
-  // const [error, setError] = useState({
-  //   question: "",
-  //   category: "",
-  // });
 
   //Get All Genres
   const GetAllGenres = async () => {
@@ -31,7 +27,8 @@ const AddAlbumForm = ({ onHide }) => {
     GetAllGenres();
   }, []);
 
-  const addAlbum = async () => {
+  const addAlbum = async (e) => {
+    e.preventDefault();
     await axios
       .post(`/album/create`, {
         title: title,
@@ -40,89 +37,94 @@ const AddAlbumForm = ({ onHide }) => {
         releaseDate: releaseDate,
       })
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
         if (res.data.success) {
-          alert("Album added successfully");
-          window.location.href = "/";
+          alert(res.data.message);
+          window.location.href = "/AlbumManager";
+        } else {
+          alert(res.data.message);
         }
       })
       .catch((err) => {
-        console.error(err);
+        alert(err.response.data.message);
       });
   };
 
   return (
-
-      <form name="form">
-        <div style={{ padding: "30px 30px 20px" }}>
-          <div className="form-floating mb-3">
-            <input
-              id="floatingInput"
-              name="AlbumTitle"
-              type="text"
-              className="form-control"
-              placeholder="Album Title"
-              onChange={(e) => setTitle(e.target.value)}
-              required
-            />
-            <label for="floatingInput">Album Title</label>
-          </div>
-
-          <div className="form-floating mb-3">
-            <input
-              id="floatingInput"
-              name="AlbumArtist"
-              type="text"
-              className="form-control"
-              placeholder="Album Artist"
-              onChange={(e) => setArtist(e.target.value)}
-              required
-            />
-            <label for="floatingInput">Album Artist</label>
-          </div>
-
-          <div className="form-outline mb-3">
-            <label className="form-label" for="form8Example5">
-              Genre
-            </label>
-            <select
-              className="form-select"
-              aria-label="Default select example"
-              id="size"
-              name="size"
-              onChange={(e) => setGenre(e.target.value)}
-            >
-              <option selected>Select Genre</option>
-              {genres.map((genres) => (
-                <option value={genres.genre} key={genres._id}>
-                  {genres.genre}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className="form-floating mb-5">
-            <input
-              id="floatingInput"
-              name="AlbumTitle"
-              type="date"
-              className="form-control"
-              placeholder="Release Date"
-              onChange={(e) => setReleaseDate(e.target.value)}
-              required
-            />
-            <label for="floatingInput">Release Date</label>
-          </div>
-
-          <hr />
-
-          <button style={{background:"#28282B"}} type="submit" className="btn btn-primary btn-block mb-4" onClick={addAlbum}>
-            PUBLISH
-          </button>
+    <form name="form">
+      <div style={{ padding: "30px 30px 20px" }}>
+        <div className="form-floating mb-3">
+          <input
+            id="floatingInput"
+            name="AlbumTitle"
+            type="text"
+            className="form-control"
+            placeholder="Album Title"
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <label for="floatingInput">Album Title</label>
         </div>
-        <div className="col"></div>
-      </form>
 
+        <div className="form-floating mb-3">
+          <input
+            id="floatingInput"
+            name="AlbumArtist"
+            type="text"
+            className="form-control"
+            placeholder="Album Artist"
+            onChange={(e) => setArtist(e.target.value)}
+            required
+          />
+          <label for="floatingInput">Album Artist</label>
+        </div>
+
+        <div className="form-outline mb-3">
+          <label className="form-label" for="form8Example5">
+            Genre
+          </label>
+          <select
+            className="form-select"
+            aria-label="Default select example"
+            id="size"
+            name="size"
+            onChange={(e) => setGenre(e.target.value)}
+          >
+            <option selected>Select Genre</option>
+            {genres.map((genres) => (
+              <option value={genres.genre} key={genres._id}>
+                {genres.genre}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-floating mb-5">
+          <input
+            id="floatingInput"
+            name="AlbumTitle"
+            type="date"
+            className="form-control"
+            placeholder="Release Date"
+            onChange={(e) => setReleaseDate(e.target.value)}
+            required
+          />
+          <label for="floatingInput">Release Date</label>
+        </div>
+
+        <hr />
+
+        <button
+          style={{ background: "#28282B" }}
+          type="submit"
+          className="btn btn-primary btn-block mb-4"
+          onClick={addAlbum}
+        >
+          PUBLISH
+        </button>
+      </div>
+      <div className="col"></div>
+    </form>
   );
 };
 
--- src/components/popup/EditAlbumForm.jsx
+++ src/components/popup/EditAlbumForm.jsx
@@ -7,7 +7,6 @@ const EditAlbumForm = ({ onHide, id }) => {
   const [genre, setGenre] = useState("");
   const [releaseDate, setReleaseDate] = useState("");
   const [genres, setGenres] = useState([]);
-  const [album, setAlbum] = useState({});
 
   //Get All Genres
   const GetAllGenres = async () => {
@@ -24,29 +23,32 @@ const EditAlbumForm = ({ onHide, id }) => {
       });
   };
 
-    //Get All Genres
-    const GetAlbem = async () => {
-        await axios
-          .get(`/album/${id}`)
-          .then((res) => {
-            if (res.data.success) {
-              const albm = res.data.album;
-              setAlbum(albm);
-            }
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-      };
+  //Get All Genres
+  const GetAlbem = async () => {
+    await axios
+      .get(`/album/${id}`)
+      .then((res) => {
+        if (res.data.success) {
+          setTitle(res.data.album.title);
+          setArtist(res.data.album.artist);
+          setGenre(res.data.album.genre);
+          setReleaseDate(res.data.album.releaseDate);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   useEffect(() => {
     GetAllGenres();
     GetAlbem();
   }, []);
 
-  const editAlbum = async () => {
+  const editAlbum = async (e) => {
+    e.preventDefault();
     await axios
-      .patch(`/album/${album._id}`, {
+      .patch(`/album/${id}`, {
         title: title,
         artist: artist,
         genre: genre,
@@ -55,90 +57,93 @@ const EditAlbumForm = ({ onHide, id }) => {
       .then((res) => {
         console.log(res);
         if (res.data.success) {
-          alert("Album Updated successfully");
+          alert("Album Updated!");
           window.location.href = "/AlbumManager";
-        }
+        } 
       })
       .catch((err) => {
-        console.error(err);
+        alert(err.response.data.message);
       });
   };
 
   return (
-
-      <form name="form">
-        <div style={{ padding: "30px 30px 20px" }}>
-          <div className="form-floating mb-3">
-            <input
-              id="floatingInput"
-              name="AlbumTitle"
-              type="text"
-              className="form-control"
-              placeholder="Album Title"
-              defaultValue={album.title}
-              onChange={(e) => setTitle(e.target.value)}
-              required
-            />
-            <label>Album Title</label>
-          </div>
-
-          <div className="form-floating mb-3">
-            <input
-              id="floatingInput"
-              name="AlbumArtist"
-              type="text"
-              className="form-control"
-              placeholder="Album Artist"
-              onChange={(e) => setArtist(e.target.value)}
-              defaultValue={album.artist}
-              required
-            />
-            <label>Album Artist</label>
-          </div>
-
-          <div className="form-outline mb-3">
-            <label className="form-label" for="form8Example5">
-              Genre
-            </label>
-            <select
-              className="form-select"
-              aria-label="Default select example"
-              id="size"
-              name="size"
-              onChange={(e) => setGenre(e.target.value)}
-            >
-              <option selected>{album.genre}</option>
-              {genres.map((genres) => (
-                <option value={genres.genre} key={genres._id}>
-                  {genres.genre}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className="form-floating mb-5">
-            <input
-              id="floatingInput"
-              name="AlbumTitle"
-              type="date"
-              className="form-control"
-              placeholder="Release Date"
-              defaultValue={album.releaseDate}
-              onChange={(e) => setReleaseDate(e.target.value)}
-              required
-            />
-            <label>Release Date</label>
-          </div>
-
-          <hr />
-
-          <button style={{background:"#28282B"}} type="submit" className="btn btn-primary btn-block mb-4" onClick={editAlbum}>
-            PUBLISH
-          </button>
+    <form name="form">
+      <div style={{ padding: "30px 30px 20px" }}>
+        <div className="form-floating mb-3">
+          <input
+            id="floatingInput"
+            name="AlbumTitle"
+            type="text"
+            className="form-control"
+            placeholder="Album Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <label>Album Title</label>
         </div>
-        <div className="col"></div>
-      </form>
 
+        <div className="form-floating mb-3">
+          <input
+            id="floatingInput"
+            name="AlbumArtist"
+            type="text"
+            className="form-control"
+            placeholder="Album Artist"
+            onChange={(e) => setArtist(e.target.value)}
+            value={artist}
+            required
+          />
+          <label>Album Artist</label>
+        </div>
+
+        <div className="form-outline mb-3">
+          <label className="form-label" for="form8Example5">
+            Genre
+          </label>
+          <select
+            className="form-select"
+            aria-label="Default select example"
+            id="size"
+            name="size"
+            onChange={(e) => setGenre(e.target.value)}
+          >
+            <option selected>{genre}</option>
+            {genres.map((genres) => (
+              <option value={genres.genre} key={genres._id}>
+                {genres.genre}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-floating mb-5">
+          <input
+            id="floatingInput"
+            name="AlbumTitle"
+            type="date"
+            className="form-control"
+            placeholder="Release Date"
+            value={releaseDate}
+            onChange={(e) => setReleaseDate(e.target.value)}
+            required
+          />
+          <label>Release Date</label>
+        </div>
+
+        <hr />
+
+        <button
+          style={{ background: "#28282B" }}
+          type="submit"
+          className="btn btn-primary btn-block mb-4"
+          onClick={editAlbum}
+        >
+          PUBLISH
+        </button>
+      </div>
+      <div className="col"></div>
+    </form>
   );
 };
 
